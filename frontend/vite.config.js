3eafb41a18995cd7a0a8e46332796826916fda32
Gabriel-1234 <59393686+Gabriel-1234@users.noreply.github.com>
2026-02-04 15:54:30 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'https://cash-app-api.up.railway.app:8080',
+        target: 'https://cash-app-api.up.railway.app',
         changeOrigin: true
       }
     }
